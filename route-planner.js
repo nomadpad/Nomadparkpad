4463bd38551d52c129e841f7805e9661e6f978ed
nomadpad <dansenetza@gmail.com>
2026-07-28 13:28:49 -0600

    location edit

--- route-planner.js
+++ route-planner.js
@@ -309,15 +309,13 @@ function waitForTripAutocomplete() {
 
 waitForTripAutocomplete();
 const tripUseLocation = document.getElementById("tripUseLocation");
-tripUseLocation?.addEventListener("pointerdown", (event) => {
+
+
+  tripUseLocation?.addEventListener("pointerdown", (event) => {
 
   event.preventDefault();
 
-});
-tripUseLocation?.addEventListener("click", () => {
-
-
-  const startInput = document.getElementById("tripStartInput");
+  const startInput = document.getElementById("tripStartInput");const startInput = document.getElementById("tripStartInput");
 
   if (!navigator.geolocation) {
 
