8f1c259c383433c586aeea6ba66f98e046326139
nomadpad <dansenetza@gmail.com>
2026-07-31 07:54:09 -0600

    center dash map

--- traveller-dashboard.js
+++ traveller-dashboard.js
@@ -8,7 +8,7 @@ function initTravellerMap() {
 
   }
 
-  const edmonton = {
+  const fallbackLocation = {
 
     lat: 53.5461,
 
@@ -16,13 +16,45 @@ function initTravellerMap() {
 
   };
 
-  new google.maps.Map(mapElement, {
+  const map = new google.maps.Map(mapElement, {
 
-    center: edmonton,
+    center: fallbackLocation,
 
     zoom: 10
 
   });
+
+  if (!navigator.geolocation) {
+
+    return;
+
+  }
+
+  navigator.geolocation.getCurrentPosition(
+
+    (position) => {
+
+      const travellerLocation = {
+
+        lat: position.coords.latitude,
+
+        lng: position.coords.longitude
+
+      };
+
+      map.setCenter(travellerLocation);
+
+      map.setZoom(11);
+
+    },
+
+    () => {
+
+      console.warn("Traveller location could not be loaded.");
+
+    }
+
+  );
 
 }
 
